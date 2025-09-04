3e86a0b087479e33921d31d419848325ff2c4896
lucagmerino <gmerinoluca@gmail.com>
2025-09-04 13:36:54 +0200

    Add files via upload

--- javascript/portfolio_js.js
+++ javascript/portfolio_js.js
@@ -1,36 +1,30 @@
-let body=document.body;
-let boton=document.getElementById("boton_byn");
-let seccion=document.getElementsByClassName("seccion_color");
+let body = document.body;
+let boton = document.getElementById("boton_byn");
+let secciones = document.querySelectorAll(".color"); // Usamos una clase fija para selección
 
+let modo_byn = false;
 
-let modo_byn=false;
-
-
-function cambioColor(){
-    if(modo_byn==false){
+function cambioColor() {
+    if (modo_byn==false) {
         body.classList.remove("bg_color");
         body.classList.add("bg_byn");
-        
-      /*  for (let i = 0; i < seccion.length; i++) {
-            seccion[i].classList.remove("seccion_color");
-            seccion[i].classList.add("seccion_byn");
-        }
 
- */
-        
+        secciones.forEach(seccion => {
+            seccion.classList.remove("seccion_color");
+            seccion.classList.add("seccion_byn");
+        });
 
-       
-        modo_byn=true;
-    }
-    else{
+        modo_byn = true;
+    } else {
         body.classList.remove("bg_byn");
-        body.classList.add("bg_color")
-        
-       
-        
-        
+        body.classList.add("bg_color");
 
-        modo_byn=false;
+        secciones.forEach(seccion => {
+            seccion.classList.remove("seccion_byn");
+            seccion.classList.add("seccion_color");
+        });
+
+        modo_byn = false;
     }
 }
 
